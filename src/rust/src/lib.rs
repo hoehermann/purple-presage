a863296c2e80fc2eaab28bd9aa82e2ca2b7156fe
Hermann Höhne <hoehermann@gmx.de>
2023-06-23 13:40:50 +0200

    Dependency issues resolved.
    Now builds, but does not run due to a lack of tokio.

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -1,4 +1,4 @@
-use futures::{channel::oneshot};
+use futures::{future, channel::oneshot};
 use presage::{
     prelude::{SignalServers,},
     Manager,
@@ -11,12 +11,12 @@ use std::ffi::CStr;
 #[no_mangle]
 pub unsafe extern "C" fn presage_link(c_device_name: *const std::os::raw::c_char) {
     let device_name: String = CStr::from_ptr(c_device_name).to_str().unwrap().to_owned();
-    println!("presage_link invoked successfully! device_name is {}", device_name);
+    println!("presage_link invoked successfully! device_name is {device_name}");
     
     // from main
-    let db_path = "presage.db";
+    let db_path = "presage";
     let passphrase: Option<String> = None;
-    println!("opening config database from {}", db_path);
+    println!("opening config database from {db_path}");
     let config_store = SledStore::open_with_passphrase(
         db_path,
         passphrase,
@@ -30,6 +30,25 @@ pub unsafe extern "C" fn presage_link(c_device_name: *const std::os::raw::c_char
         Ok(config_store) => {
             println!("config_store OK");
             let (provisioning_link_tx, provisioning_link_rx) = oneshot::channel();
+            
+            let manager = future::join(
+                Manager::link_secondary_device(
+                    config_store,
+                    servers,
+                    device_name.clone(),
+                    provisioning_link_tx,
+                ),
+                async move {
+                    match provisioning_link_rx.await {
+                        Ok(url) => {
+                            qr2term::print_qr(url.to_string()).expect("failed to render qrcode")
+                        }
+                        Err(e) => println!("Error linking device: {e}"),
+                    }
+                },
+            );
+                
+            /*
             let manager = Manager::link_secondary_device(config_store, servers, device_name.clone(), provisioning_link_tx);
             println!("now entering block_on(provisioning_link_rx)…");
             match block_on(provisioning_link_rx) {
@@ -39,10 +58,11 @@ pub unsafe extern "C" fn presage_link(c_device_name: *const std::os::raw::c_char
                 Err(e) => {
                     println!("Error linking device: {e}");
                 }
-            }
+            }*/
             println!("now entering block_on(manager)…");
+            // TODO: find out how to use tokio https://stackoverflow.com/questions/66328113/
             match block_on(manager) {
-                Ok(manager) => {
+                (Ok(manager), _) => {
                     match block_on(manager.whoami()) {
                         Ok(response) => {
                             let uuid = response.uuid;
@@ -53,7 +73,7 @@ pub unsafe extern "C" fn presage_link(c_device_name: *const std::os::raw::c_char
                         }
                     }
                 }
-                Err(err) => {
+                (Err(err), _) => {
                     println!("{err:?}");
                 }
             }
