b258c858d65fc773c6dee0c2c9a94572df460856
Hermann Höhne <hoehermann@gmx.de>
2023-07-30 19:57:34 +0200

    Correctly display remotely sent messages as sent.

--- src/c/text.c
+++ src/c/text.c
@@ -3,21 +3,27 @@
 void presage_handle_text(PurpleConnection *connection, const char *who, const char *group, uint64_t sent, uint64_t timestamp, const char *text) {
     PurpleAccount *account = purple_connection_get_account(connection);
     PurpleMessageFlags flags = 0;
-    // taken from purple-whatsmeow
-    if (purple_strequal(purple_account_get_username(account), who)) {
-        flags |= PURPLE_MESSAGE_SEND;
-        // Note: For outgoing messages (no matter if local echo or sent by other device),
-        // PURPLE_MESSAGE_SEND must be set due to how purple_conversation_write is implemented
-        if (sent) {
-            // special handling of messages sent by self incoming from remote for Spectrum
-            flags |= PURPLE_MESSAGE_REMOTE_SEND;
-        }
+    if (sent) {
+        // special handling of messages sent by self incoming from remote for Spectrum
+        // send-acknowledgements should be PURPLE_MESSAGE_SEND only (without PURPLE_MESSAGE_REMOTE_SEND)
+        // for details, look into purple-whatsmeow
+        flags |= PURPLE_MESSAGE_SEND | PURPLE_MESSAGE_REMOTE_SEND;
     } else {
         flags |= PURPLE_MESSAGE_RECV;
     }
     
     if (group == NULL) {
-        serv_got_im(connection, who, text, flags, timestamp/1000);
+        if (flags & PURPLE_MESSAGE_SEND) {
+            // display message sent from own account (other device as well as local echo)
+            // cannot use purple_serv_got_im since it sets the flag PURPLE_MESSAGE_RECV
+            PurpleConversation *conv = purple_find_conversation_with_account(PURPLE_CONV_TYPE_IM, who, account);
+            if (conv == NULL) {
+                conv = purple_conversation_new(PURPLE_CONV_TYPE_IM, account, who); // MEMCHECK: caller takes ownership
+            }
+            purple_conv_im_write(purple_conversation_get_im_data(conv), who, text, flags, timestamp/1000);
+        } else {
+            serv_got_im(connection, who, text, flags, timestamp/1000);
+        }
     } else {
         // TODO
     }
