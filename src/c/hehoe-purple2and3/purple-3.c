ad6f341769085d496845a0101c9babb3c12fa726
Hermann Höhne <hoehermann@gmx.de>
2023-08-06 13:43:38 +0200

    Small adjustments to purple_conv_im_write helper.

--- src/c/hehoe-purple2and3/purple-3.c
+++ src/c/hehoe-purple2and3/purple-3.c
@@ -1,13 +1,14 @@
 #include "purple.h"
 
 void purple_conv_im_write(PurpleConversation *conv, const char *who, const char *content, PurpleMessageFlags flags, time_t timestamp) {
-    // from pidgin-3/libpurple/protocols/facebook/util.c
+    // from pidgin3/libpurple/protocols/facebook/util.c
+    // also from pidgin3/libpurple/purpleconversation.c common_send
     PurpleAccount *account = purple_conversation_get_account(conv);
     PurpleContactInfo *info = PURPLE_CONTACT_INFO(account);
     const gchar * me = purple_contact_info_get_name_for_display(info);
-    const gchar * name = purple_account_get_username(account);
-    PurpleMessage * msg = purple_message_new_outgoing(account, me, name, content, flags);
-    GDateTime * dt = g_date_time_new_from_unix_local(timestamp); // TODO: find correct conversion
+    //const gchar * name = purple_conversation_get_name(conv);
+    PurpleMessage * msg = purple_message_new_outgoing(account, me, who, content, flags);
+    GDateTime * dt = g_date_time_new_from_unix_local(timestamp);
     purple_message_set_timestamp(msg, dt);
     g_date_time_unref(dt);
     purple_conversation_write_message(conv, msg);
