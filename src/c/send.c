3f10585ea5d6473a90cedd33d3213b53dd891826
Hermann Höhne <hoehermann@gmx.de>
2024-07-04 00:10:19 +0200

    Now with 100% less HTML in outgoing messages.

--- src/c/send.c
+++ src/c/send.c
@@ -1,22 +1,25 @@
 #include "presage.h"
 
 int presage_send_im(PurpleConnection *connection, const char *who, const char *message, PurpleMessageFlags flags) {
+    // Strip HTML similar to these reasons: https://github.com/majn/telegram-purple/issues/12 and https://github.com/majn/telegram-purple/commit/fffe751
+    char *msg = purple_markup_strip_html(message); // NOTE: This turns newlines into spaces and <br> tags into newlines
     Presage *presage = purple_connection_get_protocol_data(connection);
-    presage_rust_send_contact(rust_runtime, presage->tx_ptr, who, message);
+    presage_rust_send_contact(rust_runtime, presage->tx_ptr, who, msg);
+    g_free(msg);
     return 1; // boldly assumes message has been sent successfully
     // TODO: have various user-configurable ways of displaying success
 }
 
 int presage_send_chat(PurpleConnection *connection, int id, const gchar *message, PurpleMessageFlags flags) {
-    purple_debug_info(PLUGIN_NAME, "presage_send_chat has been called.\n");
     Presage *presage = purple_connection_get_protocol_data(connection);
     PurpleConversation *conv = purple_find_chat(connection, id);
-    purple_debug_info(PLUGIN_NAME, "PurpleConversation *conv is %p.\n", conv);
     if (conv != NULL) {
         gchar *group = (gchar *)purple_conversation_get_data(conv, "name");
-        purple_debug_info(PLUGIN_NAME, "gchar *group %s.\n", group);
         if (group != NULL) {
+            // Strip HTML similar to these reasons: https://github.com/majn/telegram-purple/issues/12 and https://github.com/majn/telegram-purple/commit/fffe751
+            char *msg = purple_markup_strip_html(message); // NOTE: This turns newlines into spaces and <br> tags into newlines
             presage_rust_send_group(rust_runtime, presage->tx_ptr, group, message);
+            g_free(msg);
             // Group chats need an explicit local echo since the implicit echo is implemented for direct messages only.
             // See https://keep.imfreedom.org/pidgin/pidgin/file/v2.14.12/libpurple/conversation.c#l191.
             // TODO: only echo locally if the message has actually been sent
