39b818aab4fd82330e8b14eeb9dc32a2acf3ccdf
Hermann Höhne <hoehermann@gmx.de>
2025-04-14 20:18:24 +0200

    Improvements for spectrum.
    * Start-up delay is user-configurable.
    * When Spectrum is detected, errors are logged and ignored instead of propagated.

--- src/c/connection.c
+++ src/c/connection.c
@@ -10,7 +10,7 @@ static gboolean rust_main_finished(gpointer account) {
     return FALSE; // tell the gtk event loop not to schedule calling this function again
 }
 
-void presage_rust_main(void *, PurpleAccount *, char *);
+void presage_rust_main(void *, PurpleAccount *, char *); // this is implemented by the rust part
 
 #ifdef WIN32
 #include <windows.h>
@@ -19,10 +19,12 @@ static DWORD WINAPI
 static void * 
 #endif 
 rust_main(void* account) {
+    // NOTE: This code is not being run on the main thread. Reading from account here is asking for trouble. Yet I am optimistic that the data will not be moved around while we are reading it.
     const char *user_dir = purple_config_dir();
     const char *username = purple_account_get_username(account);
+    const int startup_delay_seconds = purple_account_get_int(account, PRESAGE_STARTUP_DELAY_SECONDS_OPTION, 1);
     char *store_path = g_strdup_printf("%s/presage/%s", user_dir, username);
-    g_usleep(G_USEC_PER_SEC); // waiting here for a second alleviates database locking issues O_o
+    g_usleep(G_USEC_PER_SEC * startup_delay_seconds); // waiting here for alleviates database locking issues O_o
     presage_rust_main(rust_runtime, account, store_path);
     g_free(store_path);
     purple_timeout_add(500, rust_main_finished, account); // wait half a second before assessing the termination – there might be messages lingering in the rust → C bridge queue
@@ -64,4 +66,22 @@ void presage_close(PurpleConnection *connection) {
     Presage *presage = purple_connection_get_protocol_data(connection);
     presage_rust_exit(connection, rust_runtime, presage->tx_ptr);
     // TODO: deallocate protocol data (including rust channel)
+}
+
+/*
+ * This is a variant of purple_connection_error. It must be run on the main thread.
+ *
+ * The switch regarding Spectrum is necessary since Spectrum may send commands to the backend 
+ * before the backend signals readiness by explicitly setting the account to "connected".
+ */
+// TODO: Investigate why this happens, when excactly and on which commands. Then narrow down so not all errors are ignored.
+void presage_account_error(PurpleAccount *account, PurpleConnectionError reason, const char *description) {
+    GHashTable *ui_info = purple_core_get_ui_info();
+    const gchar *ui_name = g_hash_table_lookup(ui_info, "name");
+    if (purple_strequal(ui_name, "Spectrum")) {
+        purple_debug_error(PLUGIN_NAME, "Host application is Spectrum. Error is ignored: %s\n", description);
+    } else {
+        PurpleConnection *connection = purple_account_get_connection(account);
+        purple_connection_error_reason(connection, reason, description);
+    }
 }
