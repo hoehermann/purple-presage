4452323c008adcee4edb35b6232fcf9495566a8c
Hermann Höhne <hoehermann@gmx.de>
2025-10-03 22:05:43 +0200

    Convey connection error more cleanly.
    In particular, do not overwrite the actual error state with the internal communication issues.

--- src/rust/src/bridge_commands.rs
+++ src/rust/src/bridge_commands.rs
@@ -15,13 +15,7 @@ unsafe fn send_cmd(
 ) {
     if let Err(err) = send_cmd_impl(rt, tx, cmd) {
         let errmsg = std::ffi::CString::new(format!("send cmd error: {err}")).unwrap();
-        // If the receiver is disconnected in crate::core::mainloop, a network error is reported.
-        // The main loop terminates, implicitly closing the command channel immediately.
-        // On the next gtk event loop iteration, Pidgin tries to close the connection gracefully, but fails to send the exit command due to the closed channel.
-        // Consequently, the error reported here is reported as a transient network error since it can overwrite the previous error (see #18).
-        // Looking at libpurple/connection.c, purple_connection_error_reason should not overwrite a previous error, but here we are.
-        // TODO: Investigate further. Maybe use separate error reasons based on the command that was going to be sent? Or do not report an error at all, just log?
-        presage_account_error(account, crate::bridge_structs::PURPLE_CONNECTION_ERROR_OTHER_ERROR, errmsg.as_ptr());
+        presage_account_error(account, crate::bridge_structs::PURPLE_CONNECTION_ERROR_NETWORK_ERROR, errmsg.as_ptr());
     }
 }
 
