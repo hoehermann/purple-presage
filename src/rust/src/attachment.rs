20ff6efc927ae8873ff367da195f74b5756374f3
Hermann Höhne <hoehermann@gmx.de>
2026-02-06 10:27:40 +0100

    Have more compact error handling.

--- src/rust/src/attachment.rs
+++ src/rust/src/attachment.rs
@@ -4,52 +4,37 @@ pub async fn get_attachment<C: presage::store::Store + 'static>(
     attachment_pointer: presage::proto::AttachmentPointer,
     xfer: *const crate::bridge_structs::PurpleXfer,
 ) {
-    let filepath = crate::bridge::xfer_get_local_filename(xfer);
-    std::path::Path::new(&filepath).parent().and_then(|directory| std::fs::create_dir_all(directory).ok()); // TODO: error handling?
-    match tokio::fs::File::create(filepath).await {
-        Ok(mut file) => {
-            match manager.get_attachment(&attachment_pointer).await {
-                Ok(attachment_data) => match tokio::io::AsyncWriteExt::write_all(&mut file, &attachment_data).await {
-                    Ok(_) => match tokio::io::AsyncWriteExt::flush(&mut file).await {
-                        Ok(_) => crate::bridge::append_message(crate::bridge::Message {
-                            account: account,
-                            xfer: xfer,
-                            mimetype: attachment_pointer.content_type,
-                            ..Default::default() // implies default for message flag which is not an error
-                        }),
-                        Err(err) => crate::bridge::append_message(crate::bridge::Message {
-                            account: account,
-                            xfer: xfer,
-                            flags: crate::bridge_structs::PurpleMessageFlags::PURPLE_MESSAGE_ERROR,
-                            body: Some(format!("Failed to flush file due to {err}")),
-                            ..Default::default()
-                        }),
-                    },
-                    Err(err) => crate::bridge::append_message(crate::bridge::Message {
-                        account: account,
-                        xfer: xfer,
-                        flags: crate::bridge_structs::PurpleMessageFlags::PURPLE_MESSAGE_ERROR,
-                        body: Some(format!("Failed to write file due to {err}")),
-                        ..Default::default()
-                    }),
-                },
-                Err(err) => crate::bridge::append_message(crate::bridge::Message {
-                    account: account,
-                    xfer: xfer,
-                    body: Some(format!("Failed to fetch attachment due to {err}.")),
-                    flags: crate::bridge_structs::PurpleMessageFlags::PURPLE_MESSAGE_ERROR,
-                    ..Default::default()
-                }),
-            };
-        }
-        Err(err) => {
-            crate::bridge::append_message(crate::bridge::Message {
-                account: account,
-                xfer: xfer,
-                flags: crate::bridge_structs::PurpleMessageFlags::PURPLE_MESSAGE_ERROR,
-                body: Some(format!("Failed to create file due to {err}")),
-                ..Default::default()
-            });
-        }
+    let result = async {
+        let filepath = crate::bridge::xfer_get_local_filename(xfer);
+        let directory = std::path::Path::new(&filepath).parent()
+            .ok_or("Unable to get download target directory.")?;
+        std::fs::create_dir_all(directory)
+            .map_err(|err| format!("Failed to create directory due to {err}"))?;
+        let mut file = tokio::fs::File::create(&filepath).await
+            .map_err(|err| format!("Failed to create file due to {err}"))?;
+        let attachment_data = manager.get_attachment(&attachment_pointer).await
+            .map_err(|err| format!("Failed to fetch attachment due to {err}"))?;
+        tokio::io::AsyncWriteExt::write_all(&mut file, &attachment_data).await
+            .map_err(|err| format!("Failed to write file due to {err}"))?;
+        tokio::io::AsyncWriteExt::flush(&mut file).await
+            .map_err(|err| format!("Failed to flush file due to {err}"))?;
+        Ok::<(), String>(())
+    }
+    .await;
+
+    match result {
+        Ok(_) => crate::bridge::append_message(crate::bridge::Message {
+            account,
+            xfer,
+            mimetype: attachment_pointer.content_type,
+            ..Default::default()
+        }),
+        Err(err) => crate::bridge::append_message(crate::bridge::Message {
+            account,
+            xfer,
+            flags: crate::bridge_structs::PurpleMessageFlags::PURPLE_MESSAGE_ERROR,
+            body: Some(err),
+            ..Default::default()
+        }),
     }
 }
